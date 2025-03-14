11cf30269ecfb94b468ccadf294913ab08740c83
Genius Muzama <117445671+ST10027393@users.noreply.github.com>
2025-03-14 12:16:08 +0200

    First Commit
    Interface to calculate area

---
+++ ST10027393_Genius_PROG6221_IceTask3/ST10027393_Genius_PROG6221_IceTask3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ST10027393_Genius_PROG6221_IceTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }//main end
+
+        public interface ICalculateArea
+        {
+            double CalculateArea();
+        }//interface end
+
+    }
+}
